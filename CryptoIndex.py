1fb481f3c1b7f568a3a90fd610eb469b652c6b95
Eric <wzlwxpzyt@gmail.com>
2022-09-10 18:56:14 -0400

    Sept 10 Semi Finish

--- CryptoIndex.py
+++ CryptoIndex.py
@@ -1,6 +1,9 @@
-from datetime import datetime
+import json
 import pandas as pd
 import coinmarketcapapi
+import wx
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # functions
 def getSymbol(coinName):
@@ -28,23 +31,48 @@ def getListingUsingID(coinID):
   for id in coinID:
     tempDF = pd.DataFrame(cmc.cryptocurrency_quotes_latest(id=id, convert='CAD').data).transpose()
     indexData = pd.concat([indexData, tempDF])
-
+def cleanQuoteColumn(df):
+  marketCap = dict()
+  price = dict()
+  counter = 0
+  for row in df[['quote']].itertuples():
+    tempDict = json.loads(str(str(row[1])[8:len(row[1])-1]).replace("'",'"').replace('None', '0'))
+    price[coinsInMyIndex[counter]] = tempDict['price']
+    marketCap[coinsInMyIndex[counter]] = tempDict['market_cap']
+    counter += 1
+  priceTracking['price'] = list(price.values())
+  priceTracking['MarketCap'] = list(marketCap.values())
+  
 # establish connection
 cmc = coinmarketcapapi.CoinMarketCapAPI('ece06e3e-37d3-4877-808b-48ef88bb6a6a')
 
-# information about coins in my index
-coinsInMyIndex=['ETH', 'BNB', 'XRP', 'ADA', 'SOL', 'DOGE', 'DOT', 
+# information about coins in my index (no stable coins)
+# used for Indexing
+coinsInMyIndex=['BTC', 'ETH', 'BNB', 'XRP', 'ADA', 'SOL', 'DOGE', 'DOT', 
 'MATIC', 'SHIB', 'AVAX', 'UNI', 'ETC', 'LTC', 'ATOM','NEAR', 'LINK', 
-'CRO', 'XLM', 'FLOW', 'VET']
+'CRO', 'XLM', 'FLOW', 'VET'] 
 coinID=[1027,1839,52,2010,5426,74,6636,3890,5994,5805,7083,1321,2,3794,6535,1975,3635,512,4558,3077]
 
 # Add all coin information from CMC Json to DataFrame
 jsonDataBTC = cmc.cryptocurrency_quotes_latest(symbol='BTC', convert='CAD')
 indexData = pd.DataFrame(jsonDataBTC.data).transpose()
-getListing(coinsInMyIndex)
+getListingUsingID(coinID)
 
+# Cleaning API Data from CMC
 priceTracking = indexData[['name', 'symbol', 'total_supply', 'quote']]
-priceTracking['quote'] = priceTracking['quote'].to_dict()
-priceTracking['MarketCap'] = priceTracking['total_supply'].astype(float) * priceTracking['quote']['CAD']['price'] #TODO check how to get price
+cleanQuoteColumn(priceTracking)
 
-# priceTracking.to_csv('PriceData' + str(datetime.now().date()) + '.csv')
+# Index caluation
+totalIndexValue = float(input('How much would you like to put into your index: '))
+totalValue = priceTracking['MarketCap'].sum()
+tempDict = dict()
+counter = 0
+for coin in priceTracking['MarketCap']:
+  tempDict[coinsInMyIndex[counter]] = float(coin) / totalValue
+  counter += 1
+priceTracking['Prop in Index'] = list(tempDict.values())
+priceTracking['how much to buy'] = priceTracking['Prop in Index'] * 2000
+print(priceTracking['how much to buy'])
+print('saved to CryptoIndex.csv')
+
+priceTracking.to_csv('CryptoIndexSummary.csv')
