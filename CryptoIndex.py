e5aff52a23cc28d957545895c178724531b2e76d
Eric <wzlwxpzyt@gmail.com>
2022-09-10 22:05:11 -0400

    Final Revised 1a

--- CryptoIndex.py
+++ CryptoIndex.py
@@ -37,8 +37,8 @@ def cleanQuoteColumn(df):
   counter = 0
   for row in df[['quote']].itertuples():
     tempDict = json.loads(str(str(row[1])[8:len(row[1])-1]).replace("'",'"').replace('None', '0'))
-    price[coinsInMyIndex[counter]] = tempDict['price']
-    marketCap[coinsInMyIndex[counter]] = tempDict['market_cap']
+    price[counter] = tempDict['price']
+    marketCap[counter] = tempDict['market_cap']
     counter += 1
   priceTracking['price'] = list(price.values())
   priceTracking['MarketCap'] = list(marketCap.values())
@@ -47,8 +47,8 @@ def cleanQuoteColumn(df):
 cmc = coinmarketcapapi.CoinMarketCapAPI('ece06e3e-37d3-4877-808b-48ef88bb6a6a')
 
 # information about coins in my index (no stable coins)
-# used for Indexing
-coinsInMyIndex=['BTC', 'ETH', 'BNB', 'XRP', 'ADA', 'SOL', 'DOGE', 'DOT', 
+# used for Indexing, BTC already included
+coinsInMyIndex=['ETH', 'BNB', 'XRP', 'ADA', 'SOL', 'DOGE', 'DOT', 
 'MATIC', 'SHIB', 'AVAX', 'UNI', 'ETC', 'LTC', 'ATOM','NEAR', 'LINK', 
 'CRO', 'XLM', 'FLOW', 'VET'] 
 coinID=[1027,1839,52,2010,5426,74,6636,3890,5994,5805,7083,1321,2,3794,6535,1975,3635,512,4558,3077]
@@ -56,25 +56,25 @@ coinID=[1027,1839,52,2010,5426,74,6636,3890,5994,5805,7083,1321,2,3794,6535,1975
 # Add all coin information from CMC Json to DataFrame
 jsonDataBTC = cmc.cryptocurrency_quotes_latest(symbol='BTC', convert='CAD')
 indexData = pd.DataFrame(jsonDataBTC.data).transpose()
-getListingUsingID(coinID)
+getListing(coinsInMyIndex)
 
 # Cleaning API Data from CMC
 priceTracking = indexData[['name', 'symbol', 'total_supply', 'quote']]
 priceTracking.to_csv('UpdateIndexInformation.csv')
-indexInformation = pd.read_csv('UpdateIndexInformation.csv')
+indexInformation = pd.read_csv('UpdatedCoinInformation.csv')
 cleanQuoteColumn(indexInformation)
 
 # Index caluation
-totalIndexValue = float(input('How much would you like to put into your index: '))
+totalIndexValue = float(input('How much would you like to put into your index (in CAD): '))
 totalValue = priceTracking['MarketCap'].sum()
 tempDict = dict()
 counter = 0
 for coin in priceTracking['MarketCap']:
-  tempDict[coinsInMyIndex[counter]] = float(coin) / totalValue
+  tempDict[counter] = float(coin) / totalValue
   counter += 1
 priceTracking['Prop in Index'] = list(tempDict.values())
-priceTracking['how much to buy'] = priceTracking['Prop in Index'] * 2000
+priceTracking['how much to buy'] = priceTracking['Prop in Index'] * totalIndexValue
 print(priceTracking['how much to buy'])
 print('saved to CryptoIndex.csv')
 
-priceTracking.to_csv('CryptoIndexSummary.csv')
+priceTracking[['name', 'how much to buy']].to_csv('CryptoIndexSummary.csv')
