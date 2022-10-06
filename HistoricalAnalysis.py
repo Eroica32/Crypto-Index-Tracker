323a90f439f019159eb30d313e8ed97eae8896a2
Eric <wzlwxpzyt@gmail.com>
2022-10-05 21:57:02 -0400

    history extraction success

--- HistoricalAnalysis.py
+++ HistoricalAnalysis.py
@@ -3,23 +3,42 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 class HistoricalAnalysis:
-    def __init__(self, conversionRate):
+    def __init__(self, conversionRate, YFinanceData):
         self.conversionRate = conversionRate
+        self.YFinanceData = None
     def getHistory(self, targetCoin):
+        def sliceList(lst, x):
+            column = ['Date', 'BTC-CAD']
+            for i in range(x):
+                column.append(lst[i+1] + "-CAD")
+            return column
         lst = list(targetCoin['CoinName'])
+
         BTC = yf.Ticker('BTC-USD')
-        df = pd.DataFrame(BTC.history(period='max', interval = '1d'))['Date', 'Close']
-        df.columns = ['Date', 'BTC-USD']
+        df = pd.DataFrame(BTC.history(period='max', interval = '1d'))
+        df.reset_index(inplace=True)
+        df = df[['Date', 'Close']]
+        df.columns = ['Date', 'BTC-CAD']
+        df['BTC-CAD'] = df['BTC-CAD'] * self.conversionRate
 
         for i in range(len(lst)-1):
             if lst[i+1] == 'UNI':
                 temp = yf.Ticker("UNI1-USD")
-                history = pd.DataFrame(temp.history(period='max', interval='1d'))['Close']
+                history = pd.DataFrame(temp.history(period='max', interval='1d'))
+                history.reset_index(inplace=True)
+                df = df.merge(pd.DataFrame(history[['Date', 'Close']]), how='left')
+                df.columns = sliceList(lst, i+1)
+                df[str(lst[i+1]) + "-CAD"] = df[str(lst[i+1]) + "-CAD"] * self.conversionRate
+        
             else:
                 temp = yf.Ticker(str(lst[i+1]) + "-USD")
-                history = pd.DataFrame(temp.history(period='max', interval='1d'))['Close']
-            df[lst[i+1] + '-CAD'] = history * self.conversionRate
-        
-        return df
+                history = pd.DataFrame(temp.history(period='max', interval='1d'))
+                history.reset_index(inplace=True)
+                df = df.merge(pd.DataFrame(history[['Date', 'Close']]), how='left')
+                df.columns = sliceList(lst, i+1)
+                df[str(lst[i+1]) + "-CAD"] = df[str(lst[i+1]) + "-CAD"] * self.conversionRate
+                
+        self.YFinanceData = df.fillna(0)
+        return df.fillna(0)
 
 
--- Visualization.py
+++ Visualization.py
@@ -8,5 +8,8 @@ class Visualization:
         colors = sns.color_palette('bright')
         plt.pie(coreData['total coin value'], labels=coreData['symbol'],colors = colors, autopct = '%0.0f%%', labeldistance=1.3)
         plt.show()
-        def BTCvsIndex(df):
-            ...
+        
+    def BTCvsIndex(df):
+        ...
+# plot segregated bar chart to show change in market cap
+# have to calculate marketcap again each time
